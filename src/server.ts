96c305a3bb11a95532331a4ee37d6a1a1fad5a3c
Nazar Zozulya <likeemangames@gmail.com>
2025-09-12 19:05:32 +0300

    add contact me router

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,29 @@
 import express, { Express } from "express"
+import cors from "cors"
 
 const app: Express = express()
-const PORT = Number(process.env.PORT) || 3000
+const PORT = Number(process.env.PORT) || 8000
 const HOST = process.env.HOST || "localhost"
+
+app.use(express.json())
+
+app.use(cors())
 
 app.get("/", (req, res) => {
 	res.send("Hello World!")
+})
+
+interface contactMeData {
+	name: string
+	email: string
+	message: string
+}
+
+
+app.post("/contact-me/", (req, res) => {
+	const body = req.body
+	console.log(body)
+	res.json(body)
 })
 
 app.listen(PORT, HOST, () => {
