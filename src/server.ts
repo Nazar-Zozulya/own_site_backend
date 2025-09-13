9206bc51a88e20abf82e2c83d30b5a04427e6029
Nazar Zozulya <likeemangames@gmail.com>
2025-09-13 22:26:17 +0300

    add docker

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express, { Express } from "express"
 import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app: Express = express()
-const PORT = Number(process.env.PORT) || 8000
-const HOST = process.env.HOST || "localhost"
+
+const PORT = process.env.PORT ? Number(process.env.PORT) : 8000
+const HOST = process.env.HOST || "0.0.0.0" 
 
 app.use(express.json())
-
 app.use(cors())
 
 app.get("/", (req, res) => {
@@ -23,21 +26,25 @@ const BOT_TOKEN = process.env.BOT_TOKEN
 const CHAT_ID = process.env.CHAT_ID
 
 app.post("/contact-me/", async (req, res) => {
-	const body = req.body
+	try {
+		console.log(BOT_TOKEN)
+		console.log(CHAT_ID)
+		const { name, email, message }: contactMeData = req.body
+		const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`
+		const response = await fetch(url, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				chat_id: CHAT_ID,
+				text: `name: ${name} \nemail: ${email} \nmessage: ${message}`,
+			}),
+		})
 
-	const url = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`
-	const response = await fetch(url, {
-		method: "POST",
-		headers: { "Content-Type": "application/json" },
-		body: JSON.stringify({
-			chat_id: CHAT_ID,
-			text: `name: ${body.name} \nemail: ${body.email} \nmessage: ${body.message}`,
-		}),
-	})
-
-	const result = response.json()
-
-	res.json(result)
+		const result = await response.json()
+		res.json(result)
+	} catch (err) {
+		res.send(err)
+	}
 })
 
 app.listen(PORT, HOST, () => {
